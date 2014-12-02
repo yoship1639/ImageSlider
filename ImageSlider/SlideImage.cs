568ce304b9537259250eef0b9d02d8def1fc8c25
yoship1639 <yoship1639@gmail.com>
2014-12-02 15:25:54 +0900

    SlideImage構造完成

--- ImageSlider/SlideImage.cs
+++ ImageSlider/SlideImage.cs
@@ -15,9 +15,9 @@ namespace ImageSlider
     public partial class slideImage : UserControl
     {
         /// <summary>
-        /// 表示する画像の位置のレート
+        /// 表示する画像の変更度合い
         /// </summary>
-        private float rate;
+        private float rate = 0;
         public float Rate
         {
             get { return rate; }
@@ -29,14 +29,90 @@ namespace ImageSlider
             }
         }
 
-        public List<ImageData> ImageDatas { get; set; }
-        public Image ForceImage { get; set; }
+        /// <summary>
+        /// 描画する画像データリスト
+        /// </summary>
+        ImageData[] imageDatas;
+        public ImageData[] ImageDatas
+        {
+            get { return imageDatas; }
+            set
+            {
+                var prev = imageDatas;
+                imageDatas = value;
+                if (prev != value) Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// 強制的に描画する画像データ
+        /// </summary>
+        Image forceImage;
+        public Image ForceImage
+        {
+            get { return forceImage; }
+            set
+            {
+                var prev = forceImage;
+                forceImage = value;
+                if (prev != value) Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// 画像の切り替えの仕方の一覧
+        /// </summary>
+        public enum ImageSlideMode
+        {
+            Normal,
+            Slide,
+        }
+
+        /// <summary>
+        /// 画像の切り替えの仕方
+        /// </summary>
+        private ImageSlideMode slideMode = ImageSlideMode.Normal;
+        public ImageSlideMode SlideMode
+        {
+            get { return slideMode; }
+            set
+            {
+                var prev = slideMode;
+                slideMode = value;
+                if (prev != value) Invalidate();
+            }
+        }
+
+        /// <summary>
+        /// 画像のサイズモードの一覧
+        /// </summary>
+        public enum ImageSizeMode
+        {
+            Normal,
+            Stretch,
+            Center,
+            Zoom,
+            Fit,
+        }
+
+        /// <summary>
+        /// 画像のサイズモード
+        /// </summary>
+        private ImageSizeMode sizeMode = ImageSizeMode.Normal;
+        public ImageSizeMode SizeMode
+        {
+            get { return sizeMode; }
+            set
+            {
+                var prev = sizeMode;
+                sizeMode = value;
+                if (sizeMode != value) Invalidate();
+            }
+        }
 
         public slideImage()
         {
             InitializeComponent();
-
-            Rate = 0;
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -48,18 +124,65 @@ namespace ImageSlider
             // 強制的に描画する画像がある場合
             if (ForceImage != null)
             {
-               
+                g.DrawImageUnscaled(ForceImage, getImageDrawRect(ForceImage, 0, 0));
             }
 
-            // サイズ合わせ処理
+            // 画像データが無かったらリターン
+            if (imageDatas == null) return;
+
+            // 描画する画像を選択
             int no1 = (int)rate;
             int no2 = (int)rate + 1;
 
-
-            if (no1 < 0 || no1 >= ImageDatas.Count)
+            // 描画
+            if (slideMode == ImageSlideMode.Normal)         // 通常切り替え
             {
                 
             }
+
         }
+        
+        Rectangle getImageDrawRect(Image image, int x, int y)
+        {
+            if (sizeMode == ImageSizeMode.Normal)           // 通常
+            {
+                return new Rectangle(x, y, image.Width, image.Height);
+            }
+            else if (sizeMode == ImageSizeMode.Stretch)     // ストレッチ
+            {
+                return new Rectangle(x, y, Width, Height);
+            }
+            else if (sizeMode == ImageSizeMode.Center)      // センター
+            {
+                var px = (Width - image.Width) >> 1;
+                var py = (Height - image.Height) >> 1;
+                return new Rectangle(px, py, image.Width, image.Height);
+            }
+            else if (sizeMode == ImageSizeMode.Zoom)        // ズーム
+            {
+                float imgR = image.Width / (float)image.Height;
+                float ctrR = Width / (float)Height;
+
+                if (imgR > ctrR)
+                {
+
+                }
+                else if (imgR < ctrR)
+                {
+
+                }
+                else
+                {
+                    return new Rectangle(0, 0, Width, Height);
+                }
+            }
+            else                                            // フィット
+            {
+                return new Rectangle();
+            }
+            return new Rectangle(x, y, image.Width, image.Height);
+        }
+
+
     }
 }
