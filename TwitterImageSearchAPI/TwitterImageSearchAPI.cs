9137df893a9c00c7f44841c47e5249870d24753d
yoship1639 <yoship1639@gmail.com>
2014-12-03 22:23:34 +0900

    TwitterAPIを作成途中

---
+++ TwitterImageSearchAPI/TwitterImageSearchAPI.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ImageSearchAPILib;
+
+namespace TwitterImageSearchAPI
+{
+    public partial class TwitterImageSearchAPI : 
+        UserControl,
+        IImageSearchAPI
+    {
+        List<ImageData> imageDatas = new List<ImageData>();
+
+        public TwitterImageSearchAPI()
+        {
+            InitializeComponent();
+        }
+
+        public string APIName { get { return "TwitterAPI"; } }
+
+        public object[] Params
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public object[] DefaultParams
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public ImageData[] ImageDatas
+        {
+            get { return imageDatas.ToArray(); }
+        }
+
+        public int ImageCount
+        {
+            get { return imageDatas.Count; }
+        }
+
+        public event EventHandler SearchError = delegate { };
+        public event EventHandler SearchFinished = delegate { };
+        public event ImageLoadedEventHandler ImageLoaded = delegate { };
+
+        CoreTweet.Tokens tokens = null;
+
+        public void Search(string query)
+        {
+            imageDatas.Clear();
+            Task.Factory.StartNew(() =>
+            {
+                // トークンを取得
+                if (tokens == null)
+                {
+                    tokens = CoreTweet.Tokens.Create(
+                         SecretSetting.Default.ConsumerKey,
+                         SecretSetting.Default.ConsumerSecret,
+                         SecretSetting.Default.AccessToken,
+                         SecretSetting.Default.AccessTokenSecret
+                         );
+
+                    if (tokens == null)
+                    {
+                        SearchError(this, EventArgs.Empty);
+                        return;
+                    }
+                }
+
+                // 検索し、結果を取得
+                var result = tokens.Search.Tweets(new Dictionary<string, object>()
+                {
+                    {"q", "filter:images http " + query},
+                    {"include_entities", true},
+                    {"count", 100},
+                });
+            });
+            
+
+            
+        }
+
+        public ImageData GetImageData(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
