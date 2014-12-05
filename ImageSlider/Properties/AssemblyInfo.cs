560e36677537aa11e130eb90602d6013173c7c38
yoship1639 <yoship1639@gmail.com>
2014-12-06 02:51:27 +0900

    アセンブリ情報を修正

--- ImageSlider/Properties/AssemblyInfo.cs
+++ ImageSlider/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
@@ -8,9 +9,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ImageSlider")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("yoship")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ImageSlider")]
-[assembly: AssemblyCopyright("Copyright © yoship 2014")]
+[assembly: AssemblyCopyright("Copyright © Yoshihiro Ito 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("ja")]
