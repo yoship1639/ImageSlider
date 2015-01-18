7804c8331172164b474afef41c52d735edba8b72
yoship1639 <yoship1639@gmail.com>
2015-01-18 20:51:40 +0900

    versionの修正

--- ImageSlider/Properties/Settings.Designer.cs
+++ ImageSlider/Properties/Settings.Designer.cs
@@ -61,7 +61,7 @@ namespace ImageSlider.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
         public int SlideMode {
             get {
                 return ((int)(this["SlideMode"]));
